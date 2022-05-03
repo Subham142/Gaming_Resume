1f25a82d0d8fc11f220801a351cd0fb92506b2e9
Subham <subhamy142@gmail.com>
2022-05-03 11:24:12 +0530

    added melee weapon damage

--- src/attacks/MeleeWeapon.js
+++ src/attacks/MeleeWeapon.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import EffectManager from '../effects/EffectManager';
 
 class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
 
@@ -12,6 +13,7 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     this.attackSpeed = 1000;
     this.weaponAnim = weaponName + '-swing';
     this.wielder = null;
+    this.effectManager = new EffectManager(this.scene);
 
     this.setOrigin(0.5, 1);
     this.setDepth(10);
@@ -21,6 +23,7 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     this.on('animationcomplete', animation => {
       if (animation.key === this.weaponAnim) {
         this.activateWeapon(false);
+        this.body.checkCollision.none = false;
         this.body.reset(0, 0);
       }
     })
@@ -46,6 +49,12 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     this.activateWeapon(true);
     this.anims.play(this.weaponAnim, true);
 
+  }
+
+  deliversHit(target) {
+    const impactPosition = { x: this.x, y: this.getRightCenter().y };
+    this.effectManager.playEffectOn('hit-effect', target, impactPosition);
+    this.body.checkCollision.none = true;
   }
 
   activateWeapon(isActive) {
