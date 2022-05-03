8807d40a35631a4bd497fecae59b195be3126b16
Subham <subhamy142@gmail.com>
2022-05-03 11:06:01 +0530

    added melee weapon both side

--- src/attacks/MeleeWeapon.js
+++ src/attacks/MeleeWeapon.js
@@ -13,13 +13,37 @@ class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     this.weaponAnim = weaponName + '-swing';
     this.wielder = null;
 
+    this.setOrigin(0.5, 1);
+    this.setDepth(10);
+
     this.activateWeapon(false);
+
+    this.on('animationcomplete', animation => {
+      if (animation.key === this.weaponAnim) {
+        this.activateWeapon(false);
+        this.body.reset(0, 0);
+      }
+    })
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (!this.active) { return; }
+
+    if (this.wielder.lastDirection === Phaser.Physics.Arcade.FACING_RIGHT) {
+      this.setFlipX(false);
+      this.body.reset(this.wielder.x + 15, this.wielder.y);
+    } else {
+      this.setFlipX(true);
+      this.body.reset(this.wielder.x - 15, this.wielder.y);
+    }
+
   }
 
   swing(wielder) {
     this.wielder = wielder;
     this.activateWeapon(true);
-    this.body.reset(wielder.x, wielder.y);
     this.anims.play(this.weaponAnim, true);
 
   }
