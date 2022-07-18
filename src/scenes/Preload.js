9878d8d42cf5473f4d98d88598f1d3eb60da147f
Subham <subhamy142@gmail.com>
2022-07-18 22:25:08 +0530

    added klevel scroll

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -23,6 +23,8 @@ class Preload extends Phaser.Scene {
     this.load.image('bg-spikes-dark', 'assets/bg_spikes_dark.png');
     this.load.image('sky-play', 'assets/sky_play.png');
 
+    this.load.image('back', 'assets/back.png');
+
   // loading the iceball
   this.load.image('iceball-1', 'assets/weapons/iceball_001.png');
   this.load.image('iceball-2', 'assets/weapons/iceball_002.png');
@@ -85,6 +87,7 @@ class Preload extends Phaser.Scene {
 
   startGame() {
     this.registry.set('level', 1);
+    this.registry.set('unlocked-levels', 2);
     this.scene.start('MenuScene')
   }
   // create() {
