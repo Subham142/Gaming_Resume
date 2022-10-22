08d4910d6de01fcf3376f1e89ff89d46b87f97be
Subham <subhamy142@gmail.com>
2022-10-23 00:34:29 +0530

    added menu scene

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -14,7 +14,7 @@ class BaseScene extends Phaser.Scene {
   create() {
     this.add.image(0, 0, 'menu-bg')
       .setOrigin(0)
-      .setScale(2.7);
+      .setScale(1);
 
     if (this.config.canGoBack) {
       const backButton = this.add.image(this.config.width - 10, this.config.height -10, 'back')
@@ -32,7 +32,7 @@ class BaseScene extends Phaser.Scene {
     let lastMenuPositionY = 0;
 
     menu.forEach(menuItem => {
-      const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
+      const menuPosition = [this.screenCenter[0] , this.screenCenter[1] + lastMenuPositionY+50];
       menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
       lastMenuPositionY += this.lineHeight;
       setupMenuEvents(menuItem);
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -17,7 +17,7 @@ class Preload extends Phaser.Scene {
 
     this.load.image('bg-spikes-tileset', 'assets/bg_spikes_tileset.png');
 
-    this.load.image('menu-bg', 'assets/background01.png');
+    this.load.image('menu-bg', 'assets/background01.jpg');
 
     //we need to load the tiles that form up the map/tileset
     this.load.image('tiles-1', 'assets/main_lev_build_1.png');
